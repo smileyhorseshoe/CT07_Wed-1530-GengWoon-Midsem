8f857019d081c8b5b4b38023d1eeff91f2a73787
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-02 08:35:04 +0000

    Sep 2, 2026, 8:35 AM

--- testquestion2.js
+++ testquestion2.js
@@ -5,11 +5,11 @@ createCanvas(600,400);
 background(220);
 world.gravity.y = 5;
 floor = new Sprite();
-floor.color = 'black';
 floor.width = width;
 floor.x = width/2
-floor.y = 20
+floor.y = 300
 floor.collider=  'static';
+floor.color = 'black';
 
 }
 function draw() {
