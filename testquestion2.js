5c3fdb187db7653d0b956ca9d595a3d4059961a7
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-02 08:28:57 +0000

    Sep 2, 2026, 8:28 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,24 +1,24 @@
 let ball;
 let floor;
 function setup() {
-createCanvas(600,400)
-background(220)
+createCanvas(600,400);
+background(220);
 world.gravity.y = 5;
 floor = new Sprite();
-floor.collider=  'static'
-floor.x= width/2
-floor.y = 30
-floor.color = 'black'
+floor.collider=  'static';
+floor.x= width/2;
+floor.y = 30;
+floor.color = 'black';
 
 }
 function draw() {
     if (mouse.presses()) {
         ball = new Sprite();
         ball.diameter = 25;
-        ball.x = mouseX
-        ball.y = mouseY
+        ball.x = mouseX;
+        ball.y = mouseY;
     }
-    background(220)
+    background(220);
         
 
 }
