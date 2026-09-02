21aff7883b6a09305680331eff04d9985d2a3503
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-02 08:36:47 +0000

    Sep 2, 2026, 8:36 AM

--- testquestion2.js
+++ testquestion2.js
@@ -5,7 +5,7 @@ createCanvas(600,400);
 background(220);
 world.gravity.y = 5;
 floor = new Sprite();
-floor.width = width;
+floor.width = 600
 floor.x = width/2
 floor.y = 390
 floor.collider=  'static';
