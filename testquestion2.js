2f97b262942960f3654de33a2af252dc14ec76e2
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-02 08:31:21 +0000

    Sep 2, 2026, 8:31 AM

--- testquestion2.js
+++ testquestion2.js
@@ -6,7 +6,7 @@ background(220);
 world.gravity.y = 5;
 floor.x= width/2;
 floor.y = 30;
-floor = new Sprite(floor.x,floor.y,600,2);
+floor = new Sprite();
 floor.collider=  'static';
 floor.color = 'black';
 
