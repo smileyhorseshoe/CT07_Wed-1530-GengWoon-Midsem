bf0c0ef1cb0e6d10d54ceeff1fbdbc4eaef9da94
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-02 08:08:29 +0000

    Sep 2, 2026, 8:08 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,7 +1,7 @@
 let ball;
 function setup() {
 createCanvas(600,400)
-background(220)
+// background(220)
 ball = new Sprite();
 ball.diameter = 25;
 world.gravity.y = 10;
@@ -10,6 +10,6 @@ function draw() {
     if (mouse.presses()) {
         ball.x = mouseX
         ball.y = mouseY
-
+        background(220)
     }
 }
