6f2e31dd0581c5966856290e7b53e82cee174af0
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2026-09-02 08:22:29 +0000

    Sep 2, 2026, 8:22 AM

--- testquestion1.js
+++ testquestion1.js
@@ -8,12 +8,11 @@ ball.visible = false;
 world.gravity.y = 5;
 }
 function draw() {
-    background(220)
+    // background(220)
     if (mouse.presses()) {
         ball.visible = true;
         ball.x = mouseX
         ball.y = mouseY
-    ball.visible = false;
         
         
     }
